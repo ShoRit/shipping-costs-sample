16717f2cb358e547e8cd6cd04949b46cce5b2cdb
ritamdutt <ritam.dutt@gmail.com>
2017-05-21 22:26:26 +0530

    Commit message

--- app.py
+++ app.py
@@ -28,60 +28,62 @@ def webhook():
 	return r
 
 def makeWebhookResult(req):
-	if req.get("result").get("action") == 'get_action':
-		result=req.get("result")
-		parameters=result.get("parameters")
-		try:
-			contact_id=parameters.get("contact_types")
-		except:
-			contact_id=""
-		try:		
-			need_id=parameters.get("no_need")
-		except:
-			need_id=""
-		try:		
-			contact_id2=parameters.get("contact_types1")
-		except:
-			contact_id2=""	
+	try:
+		if req.get("result").get("action") == 'get_action':
+			result=req.get("result")
+			parameters=result.get("parameters")
+			try:
+				contact_id=parameters.get("contact_types")
+			except:
+				contact_id=""
+			try:		
+				need_id=parameters.get("no_need")
+			except:
+				need_id=""
+			try:		
+				contact_id2=parameters.get("contact_types1")
+			except:
+				contact_id2=""	
 
-		speech="speech\n"	
-		if contact_id=="mail":
-			speech=speech+"Here you go. My mail id is : mailme@anirbansaha.com\n"	
-		elif contact_id=="number":
-			speech=speech+"My contact number is: 1234567890\n"
-		elif contact_id=="contact":
-			speech=speech+"My email is mailme@anirbansaha.com and my phone number is 1234567890\n"
-		else:
-			speech=speech+""		
-		
-		if contact_id2=="mail":
-			speech=speech+"Here you go. My mail id is : mailme@anirbansaha.com\n"	
-		elif contact_id2=="number":
-			speech=speech+"My contact number is: 1234567890\n"
-		elif contact_id2=="contact":
-			speech=speech+"My email is mailme@anirbansaha.com and my phone number is 1234567890\n"
-		else:	
-			speech=speech+""
+			speech="speech\n"	
+			if contact_id=="mail":
+				speech=speech+"Here you go. My mail id is : mailme@anirbansaha.com\n"	
+			elif contact_id=="number":
+				speech=speech+"My contact number is: 1234567890\n"
+			elif contact_id=="contact":
+				speech=speech+"My email is mailme@anirbansaha.com and my phone number is 1234567890\n"
+			else:
+				speech=speech+""		
+			
+			if contact_id2=="mail":
+				speech=speech+"Here you go. My mail id is : mailme@anirbansaha.com\n"	
+			elif contact_id2=="number":
+				speech=speech+"My contact number is: 1234567890\n"
+			elif contact_id2=="contact":
+				speech=speech+"My email is mailme@anirbansaha.com and my phone number is 1234567890\n"
+			else:	
+				speech=speech+""
 
-		if "mail" in need_id:
-			speech=speech+"No mailid for you\n"	
-		elif "number" in need_id:
-			speech=speech+"No phone number for you\n"
-		elif "contact" in need_id:
-			speech=speech+"No contact for you\n"
-		else:
-			speech=speech+""				
+			if "mail" in need_id:
+				speech=speech+"No mailid for you\n"	
+			elif "number" in need_id:
+				speech=speech+"No phone number for you\n"
+			elif "contact" in need_id:
+				speech=speech+"No contact for you\n"
+			else:
+				speech=speech+""				
 
-		return 
-		{
-		"speech": speech,
-		"displayText": speech,
-		#"data": {},
-		# "contextOut": [],
-		"source": "apiai-onlinestore-shipping"
-		}
-	else:
-		speech="I am a Pikachu\n"
+			return 
+			{
+			"speech": speech,
+			"displayText": speech,
+			#"data": {},
+			# "contextOut": [],
+			"source": "apiai-onlinestore-shipping"
+			}
+	except Exception as error:
+		speech=str(error)
+		# speech="I am a Pikachu\n"
 		return 
 		{
 		"speech": speech,
